0d1597acd767b84fe730431ae4ab5f138b4eb643
Kai-Pin Chang <kaibin34@gmail.com>
2022-02-16 03:49:21 +0800

    code update

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,60 @@
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+
+    # Split categories into separate category columns
+    categories = df['categories'].str.split(';', expand = True)
+
+    # Use one of the rows to extract a list of names of categories and rename the columns
+    row = categories[:1].values
+    category_colnames=[]
+    for i in range(36):
+        category_colnames.append(row[0,i][:-2])
+    categories.columns = category_colnames
+
+    #Convert category values to just numbers 0 or 1.
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = pd.to_numeric(categories[column])
+
+    #Replace categories column in df with new category columns
+    df.drop(['categories'], axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis = 1)
+
+    #Remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    #Modify the column "related" into "unrelated"
+    df['related']=df['related'].map(lambda x: 1 if x == 2 else x)
+    df['related']=df['related'].map(lambda x: 1 if x == 0 else 0)
+    df.rename(columns={"related":"unrelated"}, inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+
+    #Save the clean dataset into an sqlite database
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    db_file_name = database_filename.split("/")[-1] # extract file name from \
+                                                     # the file path
+    table_name = db_file_name.split(".")[0]
+    df.to_sql(table_name, engine, index=False, if_exists = 'replace')
+
+    pass
 
 
 def main():
@@ -24,12 +68,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,23 +1,105 @@
+# import libraries
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
+import pandas as pd
+import numpy as np
+
+from sqlalchemy import create_engine
+
+import re
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+
+import pickle
+
+import warnings
+warnings.filterwarnings('ignore')
 
 def load_data(database_filepath):
-    pass
+
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('DisasterResponse', engine)
+
+    # the column "related" contains value 2, which can lead to error
+    # thus changing them to 1
+    # df['related']=df['related'].map(lambda x: 1 if x == 2 else x)
+
+    # split the data into features(X) and targets(y, column_names)
+    X = df.message
+    y = df.iloc[:,4:]
+    category_names = y.columns
+
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
 
+    # keep only lower case alphabets and numbers in the text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize the words (including lemmatizing and stripping)
+    tokens = word_tokenize(text)
+    lemmatized = [WordNetLemmatizer().lemmatize(tok).strip() for tok in tokens]
+
+    # remove stop words
+    stopwords_ = stopwords.words("english")
+    cleantokens = [word for word in lemmatized if word not in stopwords_]
+
+    return cleantokens
 
 def build_model():
-    pass
+
+    # build pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    # MultiOutputClassifier(RandomForestClassifier())
+
+    # hyper-parameter
+    parameters = {'clf__estimator__learning_rate': (2.0, 1.0, 0.5)
+                  }
+
+    # create model
+    model = GridSearchCV(estimator=pipeline,
+            param_grid=parameters)
+
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+
+    y_pred_test = model.predict(X_test)
+    test = classification_report(Y_test.values, y_pred_test, target_names = category_names)
+    print(test)
+
+    # print accuracy score
+    # print('Accuracy: {}'.format(np.mean(Y_test.values == y_pred_test)))
+
     pass
 
 
 def save_model(model, model_filepath):
+
+    with open(model_filepath, 'wb') as classifier:
+        pickle.dump(model, classifier)
+
     pass
 
 
@@ -26,14 +108,14 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
